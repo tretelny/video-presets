28f5be533355e74ebbd3c760c6dee08edd8976c7
Francisco Souza <francisco.souza@nytimes.com>
2016-10-25 17:56:45 -0400

    apply_presets: make script more robust
    Validate parameters and strip trailing slashes.

--- apply_presets.sh
+++ apply_presets.sh
@@ -1,3 +1,12 @@
+#!/bin/bash -e
+
+if [ -z "$1" ]; then
+  echo >&2 "please specify the URL of the transcoding-api"
+  exit 2
+fi
+
+endpoint=$(echo $1 | sed -e 's;/*$;;')
+
 for f in *.json; do
-  curl -X POST -d @$f $1/presets;
+  curl -X POST -d @${f} ${endpoint}/presets;
 done
